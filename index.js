edd670763748c98707cd083b0ae1de7b0442f6a3
Ivlina Moroshkina <ivlina@Ivlinas-MacBook-Air.local>
2023-03-29 15:26:34 +0300

    Main homework is ready.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const learnJS = () => console.log(`Я учу JavaScript!`);
 learnJS();
 
+const changePicturePrevious = () => {
+    let egyptPicture = document.getElementById('photo');
+    egyptPicture.src = 'images/ibn_tulun.jpeg';
+};
+
+const changePictureNext = () => {
+    let egyptPicture = document.getElementById('photo');
+    egyptPicture.src = 'images/aswan.jpeg';
+};
